bee02ef5629b5102b81f5f37288833b172e4d1a1
HC5MAC11 <lucas.sirodrigues@gmail.com>
2017-09-21 19:58:51 -0300

    Initial Commit

---
+++ Aula7/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Aula7
+//
+//  Created by HC5MAC11 on 21/09/17.
+//  Copyright © 2017 Lucas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
