6e14ebb8456b24a08533d7d98085ba857deaecaa
Lucas Rodrigues <lucas.sirodrigues@gmail.com>
2017-10-26 21:01:31 -0300

    Validando os campos do formulario

--- Aula7/NewUserViewController.swift
+++ Aula7/NewUserViewController.swift
@@ -19,7 +19,7 @@ class NewUserViewController: UITableViewController {
     @IBOutlet weak var siteField: UITextField!
     
     @IBAction func saveButton(_ sender: UIBarButtonItem) {
-        self.saveCoreData()
+        self.validateContact()
         self.dismiss(animated: true, completion: nil)
     }
     @IBAction func cancelButton(_ sender: UIBarButtonItem) {
@@ -43,6 +43,102 @@ class NewUserViewController: UITableViewController {
     private var session: URLSession {
         let session = URLSession(configuration: configuration, delegate: self, delegateQueue: operationQueue)
         return session
+    }
+    
+    func validateContact() {
+        
+        var isOk = true
+        
+        if loginField.text == nil || loginField.text! == ""{
+            isOk = false
+            loginField.layer.backgroundColor = UIColor.red.cgColor
+        }else{
+            loginField.layer.backgroundColor = nil
+        }
+        if nameField.text == nil || nameField.text! == ""{
+            isOk = false
+            nameField.layer.backgroundColor = UIColor.red.cgColor
+        }else{
+            nameField.layer.backgroundColor = nil
+        }
+        if emailField.text == nil || emailField.text! == "" || !emailField.text!.contains("@"){
+            isOk = false
+            emailField.layer.backgroundColor = UIColor.red.cgColor
+        }else{
+            emailField.layer.backgroundColor = nil
+        }
+        if phoneField.text == nil || phoneField.text! == ""{
+            isOk = false
+            phoneField.layer.backgroundColor = UIColor.red.cgColor
+        }else{
+            phoneField.layer.backgroundColor = nil
+        }
+        if siteField.text == nil || siteField.text! == "" || !(siteField.text!.contains("http://") || siteField.text!.contains("https://")){
+            isOk = false
+            siteField.layer.backgroundColor = UIColor.red.cgColor
+        }else{
+            siteField.layer.backgroundColor = nil
+        }
+//        if streetTextField.text == nil || streetTextField.text! == ""{
+//            isOk = false
+//            streetTextField.layer.backgroundColor = UIColor.red.cgColor
+//        }else{
+//            streetTextField.layer.backgroundColor = nil
+//        }
+//        if suiteTextField.text == nil || suiteTextField.text! == ""{
+//            isOk = false
+//            suiteTextField.layer.backgroundColor = UIColor.red.cgColor
+//        }else{
+//            suiteTextField.layer.backgroundColor = nil
+//        }
+//        if cityTextField.text == nil || cityTextField.text! == ""{
+//            isOk = false
+//            cityTextField.layer.backgroundColor = UIColor.red.cgColor
+//        }else{
+//            cityTextField.layer.backgroundColor = nil
+//        }
+//        if zipCodeTextField.text == nil || zipCodeTextField.text! == ""{
+//            isOk = false
+//            zipCodeTextField.layer.backgroundColor = UIColor.red.cgColor
+//        }else{
+//            zipCodeTextField.layer.backgroundColor = nil
+//        }
+//        if latitudeTextField.text == nil || latitudeTextField.text! == ""{
+//            isOk = false
+//            latitudeTextField.layer.backgroundColor = UIColor.red.cgColor
+//        }else{
+//            latitudeTextField.layer.backgroundColor = nil
+//        }
+//        if longitudeTextField.text == nil || longitudeTextField.text! == ""{
+//            isOk = false
+//            longitudeTextField.layer.backgroundColor = UIColor.red.cgColor
+//        }else{
+//            longitudeTextField.layer.backgroundColor = nil
+//        }
+//        if companyTextField.text == nil || companyTextField.text! == ""{
+//            isOk = false
+//            companyTextField.layer.backgroundColor = UIColor.red.cgColor
+//        }else{
+//            companyTextField.layer.backgroundColor = nil
+//        }
+//        if catchPhraseTextField.text == nil || catchPhraseTextField.text! == ""{
+//            isOk = false
+//            catchPhraseTextField.layer.backgroundColor = UIColor.red.cgColor
+//        }else{
+//            catchPhraseTextField.layer.backgroundColor = nil
+//        }
+//        if bdTextField.text == nil || bdTextField.text! == ""{
+//            isOk = false
+//            bdTextField.layer.backgroundColor = UIColor.red.cgColor
+//        }else{
+//            bdTextField.layer.backgroundColor = nil
+//        }
+        
+        if(isOk){
+            saveCoreData()
+            self.dismiss(animated: true, completion: nil)
+        }
+        
     }
     
     private func saveCoreData() {
